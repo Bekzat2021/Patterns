8e700f5d6ff0293d492a72690bf7c664d6ee3a80
Bekzat2021 <bekzat.work@gmail.com>
2020-06-28 20:18:35 +0500

    Thread safe singleton

---
+++ PatternsApp/President.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PatternsApp
+{
+    class President
+    {
+        private static President instance;
+        public string Name { get; private set; }
+
+        protected President(string name)
+        {
+            Name = name;
+        }
+
+        public static President GetInstance(string name)
+        {
+            if (instance == null)
+                instance = new President(name);
+            return instance;
+        }
+    }
+
+    class Country
+    {
+        public President President { get; set; }
+
+        public void Elect(string name)
+        {
+            President = President.GetInstance(name);
+        }
+    }
+}
---
+++ PatternsApp/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Threading;
+
+namespace PatternsApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            (new Thread(() =>
+            {
+                Country Mexico = new Country();
+                Mexico.Elect("Pedro");
+                Console.WriteLine(Mexico.President.Name);
+            })).Start();
+
+            Country USA = new Country();
+            USA.Elect("Trump");
+
+            Console.WriteLine(USA.President.Name);
+
+            #region
+            //(new Thread(() =>
+            //{
+            //    Computer comp2 = new Computer();
+            //    comp2.OS = OS.GetInstance("Windows 10");
+            //    Console.WriteLine(comp2.OS.Name);
+            //})).Start();
+
+            //Computer comp = new Computer();
+            //comp.Launch("Windows 8.1");
+            //Console.WriteLine(comp.OS.Name);
+            #endregion
+        }
+    }
+
+    class Computer
+    {
+        public OS OS { get; set; }
+        public void Launch(string osName)
+        {
+            OS = OS.GetInstance(osName);
+        }
+    }
+
+    class OS
+    {
+        private static OS instance;
+        public string Name { get; private set; }
+        private static object syncRoot = new object();
+
+        protected OS(string name) 
+        {
+            this.Name = name;
+        }
+
+        public static OS GetInstance(string name)
+        {
+            if (instance == null)
+            {
+                lock (syncRoot)
+                {
+                    if (instance == null)
+                        instance = new OS(name);
+                }
+            }
+                
+            return instance;
+        }
+    }
+}
