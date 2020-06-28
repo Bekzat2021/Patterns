e3e3ca4f79f5d5c59321b7c0c4f2da70e8eb390d
Bekzat2021 <bekzat.work@gmail.com>
2020-06-28 21:00:59 +0500

    Singleton Lazy<T> realization

--- PatternsApp/Program.cs
+++ PatternsApp/Program.cs
@@ -7,65 +7,26 @@ namespace PatternsApp
     {
         static void Main(string[] args)
         {
-            (new Thread(() =>
-            {
-                Country Mexico = new Country();
-                Mexico.Elect("Pedro");
-                Console.WriteLine(Mexico.President.Name);
-            })).Start();
+            Singleton singleton = Singleton.GetInstance();
+            Console.WriteLine(singleton.Name);
 
-            Country USA = new Country();
-            USA.Elect("Trump");
-
-            Console.WriteLine(USA.President.Name);
-
-            #region
-            //(new Thread(() =>
-            //{
-            //    Computer comp2 = new Computer();
-            //    comp2.OS = OS.GetInstance("Windows 10");
-            //    Console.WriteLine(comp2.OS.Name);
-            //})).Start();
-
-            //Computer comp = new Computer();
-            //comp.Launch("Windows 8.1");
-            //Console.WriteLine(comp.OS.Name);
-            #endregion
+            Singleton singleton2 = Singleton.GetInstance();
+            Console.WriteLine(singleton2.Name);
         }
     }
 
-    class Computer
+    class Singleton
     {
-        public OS OS { get; set; }
-        public void Launch(string osName)
-        {
-            OS = OS.GetInstance(osName);
-        }
-    }
-
-    class OS
-    {
-        private static OS instance;
+        private static readonly Lazy<Singleton> lazy = new Lazy<Singleton>(() => new Singleton());
         public string Name { get; private set; }
-        private static object syncRoot = new object();
-
-        protected OS(string name) 
+        public Singleton()
         {
-            this.Name = name;
+            Name = System.Guid.NewGuid().ToString();
         }
 
-        public static OS GetInstance(string name)
+        public static Singleton GetInstance()
         {
-            if (instance == null)
-            {
-                lock (syncRoot)
-                {
-                    if (instance == null)
-                        instance = new OS(name);
-                }
-            }
-                
-            return instance;
+            return lazy.Value;
         }
     }
 }
