74baffab170431fdcc121abaff848442b9620d6c
Bekzat2021 <bekzat.work@gmail.com>
2020-07-03 23:37:17 +0500

    Prototype pattern

--- PatternsApp/Program.cs
+++ PatternsApp/Program.cs
@@ -7,26 +7,114 @@ namespace PatternsApp
     {
         static void Main(string[] args)
         {
-            Singleton singleton = Singleton.GetInstance();
-            Console.WriteLine(singleton.Name);
+            //IFigure figure = new Rectangle(30, 40);
+            //IFigure clonedFigure = figure.Clone();
+            //figure.GetInfo();
+            //clonedFigure.GetInfo();
 
-            Singleton singleton2 = Singleton.GetInstance();
-            Console.WriteLine(singleton2.Name);
+            //IFigure circle = new Circle(30);
+            //var clonedCircle = circle.Clone();
+            //circle.GetInfo();
+            //clonedCircle.GetInfo();
+
+            IUser admin = new Admin("Admin01");
+            var admin02 = admin.Clone();
+            admin.Info();
+            admin02.Info();
+
+
+            IUser player = new Player("Ken0235");
+            IUser player5 = player.Clone();
+            player.Info();
+            player5.Info();
         }
     }
 
-    class Singleton
+    interface IFigure
     {
-        private static readonly Lazy<Singleton> lazy = new Lazy<Singleton>(() => new Singleton());
-        public string Name { get; private set; }
-        public Singleton()
+        IFigure Clone();
+        void GetInfo();
+    }
+
+    class Rectangle : IFigure
+    {
+        int width;
+        int height;
+
+        public Rectangle(int w, int h)
         {
-            Name = System.Guid.NewGuid().ToString();
+            width = w;
+            height = h;
+        }
+        public IFigure Clone()
+        {
+            return new Rectangle(this.width, this.height);
         }
 
-        public static Singleton GetInstance()
+        public void GetInfo()
         {
-            return lazy.Value;
+            Console.WriteLine($"Прямоугольник длиной {height} и шириной {width}");
+        }
+    }
+
+    class Circle : IFigure
+    {
+        int radius;
+        public Circle(int r)
+        {
+            radius = r;
+        }
+        public IFigure Clone()
+        {
+            return new Circle(this.radius);
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Круг радиусом {radius}");
+        }
+    }
+
+    interface IUser
+    {
+        IUser Clone();
+        void Info();
+    }
+
+    class Admin : IUser
+    {
+        public string Name { get; set; }
+        public Admin(string name)
+        {
+            Name = name;
+        }
+
+        public IUser Clone()
+        {
+            return new Admin(Name);
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"My name is {Name}");
+        }
+    }
+
+    class Player : IUser
+    {
+        public string Nickname { get; set; }
+        public Player(string nickname)
+        {
+            Nickname = nickname;
+        }
+        public IUser Clone()
+        {
+            return new Player(Nickname);
+        }
+
+        public void Info()
+        {
+            Console.WriteLine($"My Nickname is {Nickname}");
         }
     }
 }
