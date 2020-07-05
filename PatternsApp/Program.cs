b806ba29d53030a831fde29f52c41d69ce23d6d6
Bekzat2021 <bekzat.work@gmail.com>
2020-07-05 15:42:14 +0500

    Observer pattern

--- PatternsApp/Program.cs
+++ PatternsApp/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Net.Http;
 using System.Runtime.Serialization;
@@ -13,62 +14,185 @@ namespace PatternsApp
     {
         static void Main(string[] args)
         {
-            Navigator navigator = new Navigator("аэропорт", "дом", new Walk());
-            navigator.Move();
+            //Stock stock = new Stock();
+            //Bank bank = new Bank("Unity bank", stock);
+            //Broker broker = new Broker("John SMith", stock);
 
-            navigator.MoveType = new onCar();
-            navigator.Move();
+            //stock.Market();
+            //broker.StopTrade();
+            //Console.WriteLine();
 
-            navigator.MoveType = new OnBus();
-            navigator.Move();
+            //stock.Market();
+
+            Engineer engineer = new Engineer();
+            Viewer smith = new Viewer();
+            Viewer bob = new Viewer();
+
+            engineer.RegisterViewer(smith);
+            engineer.RegisterViewer(bob);
+            engineer.Make(false);
         }
     }
 
-    interface IMove
+    interface IEngineer
     {
-        void Move();
+        void RegisterViewer(IViewer viewer);
+        void RemoveViewer(IViewer viewer);
+        void NotifyViewers(bool cool);
     }
 
-    class Walk : IMove
+    interface IViewer
     {
-        public void Move()
-        {
-            Console.WriteLine("пешком");
-        }
+        void Update(bool cool);
     }
 
-    class onCar : IMove
+    class Engineer : IEngineer
     {
-        public void Move()
+        List<IViewer> viewers;
+        public Engineer()
         {
-            Console.WriteLine("на автомобиле");
+            viewers = new List<IViewer>();
+        }
+        public void NotifyViewers(bool cool)
+        {
+            foreach (var item in viewers)
+            {
+                item.Update(cool);
+            }
+        }
+
+        public void RegisterViewer(IViewer viewer)
+        {
+            viewers.Add(viewer);
+        }
+
+        public void RemoveViewer(IViewer viewer)
+        {
+            viewers.Remove(viewer);
+        }
+
+        public void Make(bool cool)
+        {
+            NotifyViewers(cool);
         }
     }
 
-    class OnBus : IMove
+    class Viewer : IViewer
     {
-        public void Move()
+        public void Update(bool cool)
         {
-            Console.WriteLine("на автобусе");
+            if (cool)
+                Console.WriteLine("Engineer make something cool");
+            else
+                Console.WriteLine("Engineer make something NOT cool");
         }
     }
 
-    class Navigator
+    #region
+    interface IObserver
     {
-        public string Start { get; set; }
-        public string Finish { get; set; }
-        public IMove MoveType { get; set; }
-        public Navigator(string start, string finish, IMove moveType)
+        void Update(Object obj);
+    }
+
+    interface IObservable
+    {
+        void RegisterObserver(IObserver observer);
+        void RemoveObserver(IObserver observer);
+        void NotifyObservers();
+    }
+
+    class Stock : IObservable
+    {
+        StockInfo sInfo;
+        List<IObserver> observers;
+
+        public Stock()
         {
-            Start = start;
-            Finish = finish;
-            MoveType = moveType;
+            observers = new List<IObserver>();
+            sInfo = new StockInfo();
         }
 
-        public void Move()
+        public void Market()
         {
-            Console.Write($"Вы перемещаетесть из {Start} в {Finish} ");
-            MoveType.Move();
+            Random rnd = new Random();
+            sInfo.USD = rnd.Next(20, 40);
+            sInfo.Euro = rnd.Next(30, 50);
+            NotifyObservers();
+        }
+
+        public void RegisterObserver(IObserver observer)
+        {
+            observers.Add(observer);
+        }
+
+        public void RemoveObserver(IObserver observer)
+        {
+            observers.Remove(observer);
+        }
+        public void NotifyObservers()
+        {
+            foreach (var observer in observers)
+            {
+                observer.Update(sInfo);
+            }
         }
     }
+
+    class Broker : IObserver
+    {
+        public string Name { get; set; }
+        IObservable stock;
+
+        public Broker(string name, IObservable observable)
+        {
+            Name = name;
+            stock = observable;
+            stock.RegisterObserver(this);
+        }
+
+
+        public void Update(object obj)
+        {
+            StockInfo sInfo = (StockInfo)obj;
+
+            if(sInfo.USD > 30)
+                Console.WriteLine($"Брокер {Name} продает доллары;  Курс доллара: {sInfo.USD}");
+            else
+                Console.WriteLine($"Брокер {Name} покупает доллары;  Курс доллара: {sInfo.USD}");
+        }
+
+        public void StopTrade()
+        {
+            stock.RemoveObserver(this);
+            stock = null;
+        }
+    }
+
+    class Bank : IObserver
+    {
+        public string Name { get; set; }
+        IObservable stock;
+        public Bank(string name, IObservable stock)
+        {
+            Name = name;
+            this.stock = stock;
+            stock.RegisterObserver(this);
+        }
+        public void Update(object obj)
+        {
+            StockInfo sInfo = (StockInfo)obj;
+
+            if(sInfo.Euro > 40)
+                Console.WriteLine($"Банк {Name} продает евро;  Курс евро: {sInfo.Euro}");
+            else
+                Console.WriteLine($"Банк {Name} покупает евро;  Курс евро: {sInfo.Euro}");
+        }
+    }
+
+    class StockInfo
+    {
+        public int USD { get; set; }
+        public int Euro { get; set; }
+    }
+    #endregion
 }
