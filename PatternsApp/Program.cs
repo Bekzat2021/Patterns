e5a768ce7991287961e7a72c822584df5f991c88
Bekzat2021 <bekzat.work@gmail.com>
2020-07-21 20:57:24 +0600

    Iterator pattern ended

--- PatternsApp/Program.cs
+++ PatternsApp/Program.cs
@@ -16,23 +16,23 @@ namespace PatternsApp
             Reader reader = new Reader();
             reader.SeeBooks(library);
 
-            Week days = new Week();
+            Week week = new Week();
             User user = new User();
-            user.SeeDays();
+            user.SeeWeek(week);
         }
     }
 
-    interface IDaysIterator
+    class User
     {
-        bool HasNext();
-        Day Next();
-    }
-
-    interface IDaysEnumerable
-    {
-        Day this[int index] { get; }
-        int Count { get; }
-        IDaysIterator CreateNumerator();
+        public void SeeWeek(Week week)
+        {
+            IDayIterator iterator = week.CreateIterator();
+            while (iterator.HasNext())
+            {
+                Day day = iterator.Next();
+                Console.WriteLine(day.Name);
+            }
+        }
     }
 
     class Day
@@ -40,12 +40,140 @@ namespace PatternsApp
         public string Name { get; set; }
     }
 
-    class Week
+    interface IDayIterator
+    {
+        bool HasNext();
+        Day Next();
+    }
+
+    interface IDayNumerable
+    {
+        IDayIterator CreateIterator();
+        int Count { get; }
+        Day this[int index] { get; } //индексатор
+    }
+
+    class Week : IDayNumerable
     {
         Day[] days;
         public Week()
         {
-            //days = Закончить
+            days = new Day[]{ new Day { Name = "Monday" },
+                new Day { Name = "Tuesday" },
+                new Day { Name = "Wednesday" },
+                new Day { Name = "Thursday" },
+                new Day { Name = "Friday" },
+                new Day { Name = "Saturday" },
+                new Day { Name = "Sunday" }};
+        }
+
+        public Day this[int index] => days[index];
+
+        public int Count => days.Length;
+
+        public IDayIterator CreateIterator()
+        {
+            return new WeekIterator(this);
+        }
+    }
+
+    class WeekIterator : IDayIterator
+    {
+        IDayNumerable numerable;
+        int index = 0;
+        public WeekIterator(IDayNumerable n)
+        {
+            numerable = n;
+        }
+
+        public bool HasNext()
+        {
+            return index < numerable.Count;
+        }
+
+        public Day Next()
+        {
+            return numerable[index++];
+        }
+    }
+
+    //класс читателя *************************************************************
+    class Reader
+    {
+        public void SeeBooks(Library library)
+        {
+            IBookIterator iterator = library.CreateNumerator();
+            while (iterator.HasNext())
+            {
+                Book book = iterator.Next();
+                Console.WriteLine(book.Name);
+            }
+        }
+    }
+    //класс книги с свойством имени
+    class Book
+    {
+        public string Name { get; set; }
+    }
+    //итератор 
+    interface IBookIterator
+    {
+        bool HasNext();
+        Book Next();
+    }
+
+    interface IBookNumerable
+    {
+        IBookIterator CreateNumerator();
+        int Count { get; }
+        Book this[int index] { get; }
+    }
+
+    class Library : IBookNumerable
+    {
+        private Book[] books;
+        public Library()
+        {
+            books = new Book[]
+            {
+                new Book{ Name = "Война и мир" },
+                new Book { Name = "Отцы и дети" },
+                new Book { Name = "Вишневый сад" }
+            };
+        }
+
+        public int Count
+        {
+            get { return books.Length; }
+        }
+
+        public Book this[int index]
+        {
+            get { return books[index]; }
+        }
+
+        public IBookIterator CreateNumerator()
+        {
+            return new LibraryNumertor(this);
+        }
+    }
+
+    class LibraryNumertor : IBookIterator
+    {
+        IBookNumerable aggregate;
+        int index = 0;
+        public LibraryNumertor(IBookNumerable a)
+        {
+            aggregate = a;
+        }
+        public bool HasNext()
+        {
+            return index < aggregate.Count;
+        }
+
+        public Book Next()
+        {
+            return aggregate[index++];
         }
     }
 }
